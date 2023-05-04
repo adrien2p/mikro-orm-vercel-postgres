1375fe35692161da7d4183f306ae5a8828ec829f
adrien2p <adrien.deperetti@gmail.com>
2023-05-04 16:34:22 +0200

    wip prisma test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,9 +10,11 @@ export const config = {
   regions: ['iad1'], // only execute this function on iad1
 };
 
-export default async (request: NextRequest) => {
-  const client = new PrismaClient()
-  const data = await client.test.findMany()
+(global as any).client = (global as any).client ?? new PrismaClient()
+
+export default async function handler (request: NextRequest) {
+
+  const data = await (global as any).client.test.findMany()
 
   return NextResponse.json({
     name: `Hello, from ${request.url} I'm now an Edge Function!`,

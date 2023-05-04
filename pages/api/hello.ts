b5b3682abb602c73030fc4d355345e0b9e1bbe5d
adrien2p <adrien.deperetti@gmail.com>
2023-05-04 16:18:40 +0200

    test query

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm/browser"
 
 import { NextRequest, NextResponse, } from 'next/server';
 

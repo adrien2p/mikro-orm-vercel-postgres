3af314ff09c1972267b82b0a08a098a3bc40f086
adrien2p <adrien.deperetti@gmail.com>
2023-05-04 16:08:54 +0200

    test typeorm

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,16 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-import {MikroORM} from "@mikro-orm/core";
-import { Test } from '@/dist-db/entities/test';
+import { DataSource } from "typeorm/browser"
 
 type Data = any
-
-/*
-Object.assign(require("knex/lib/dialects/postgres").prototype, {
-  _driver: require("@neondatabase/serverless").Pool,
-});
-*/
 
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -24,26 +16,5 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
 
-  const ormConfig = {
-    dbName: process.env.POSTGRES_DATABASE,
-    debug: process.env.APP_ENV === 'development',
-    entities: [Test],
-    baseDir: process.cwd(),
-    host: process.env.POSTGRES_HOST,
-    driverOptions: {
-      connection: { ssl: true },
-    },
-    password: process.env.POSTGRES_PASSWORD,
-    port: Number(process.env.POSTGRES_PORT),
-    tsNode: process.env.APP_ENV === 'development',
-    type: 'postgresql',
-    user: process.env.POSTGRES_USER
-  } as any
-
-  const orm = await MikroORM.init(ormConfig)
-
-  const em = orm.em.fork()
-  const tests = await em.find(Test, {});
-
-  res.status(200).json(tests)
+  res.status(200).json({})
 }

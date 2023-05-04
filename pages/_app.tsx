e2bab28c1d1616070471db106bf1d9d9306ace7d
adrien2p <adrien.deperetti@gmail.com>
2023-05-04 17:24:06 +0200

    wip vercel db test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export const preferredRegion = 'home'
-export const dynamic = 'force-dynamic'
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }

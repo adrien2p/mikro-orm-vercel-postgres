0f6ef6d1efabeba399ce14f2e68e311c678d1099
adrien2p <adrien.deperetti@gmail.com>
2023-05-05 12:21:25 +0200

    comment prisma related

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,4 @@
+/*
 import { PrismaClient } from '@prisma/client'
 
 declare global {
@@ -8,4 +9,4 @@ const prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
-export default prisma
+export default prisma*/

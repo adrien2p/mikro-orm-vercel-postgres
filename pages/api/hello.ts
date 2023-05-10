c48f2794f1917c0e3002255aede6890cf32527f2
adrien2p <adrien.deperetti@gmail.com>
2023-05-10 11:22:02 +0200

    test global

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,24 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { initialize as ProductModuleInitialize } from "@medusajs/product"
+import { initialize as ProductModuleInitialize, default as test } from "@medusajs/product"
 
 export const config = {
   /*runtime: 'edge', // this is a pre-requisite
   regions: ['iad1'], // only execute this function on iad1*/
 };
 
-const productModule = await ProductModuleInitialize({
-  database: {
-    clientUrl: process.env.POSTGRES_URL!,
-    schema: "public",
-  }
-})
+declare global {
+  var productModule: any
+}
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
+  global.productModule = global.productModule ?? await ProductModuleInitialize({
+    database: {
+      clientUrl: process.env.POSTGRES_URL!,
+      schema: "public",
+    }
+  })
   const data = await productModule.listVariants()
 
   res.status(200).json(data)
